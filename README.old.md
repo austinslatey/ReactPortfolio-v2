60ad98b89f45bac5167a8829dbc0542b21dd43d8
Trappin Recidivism <austinslater58@gmail.com>
2023-02-02 17:13:43 -0600

    Inital file structure achieved, conditonal rendering for navigation though pages successful

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# reactPortfolio-v2
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Contact from "./pages/Contact/Contact";
+
+//import Footer from "./Footer";
+
+
+
+
+
+
+
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("Home");
+
+ 
+  // checking each condition to see what page we are on , then rendering that page, otherwise rendering the contact form
+  const renderPage = () => {
+    if (currentPage === "Home") {
+      return <Home />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    // if (currentPage === "ContactForm") return <ContactForm />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <div>
+      {/* the handlepage function is what were using as prop */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
+      </div>
+      {/* <Footer /> */}
+    </div>
+  );
+}
