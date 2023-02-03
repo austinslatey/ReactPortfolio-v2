4cb0981ccd695fe5b705810e8a8d5ceb6db3f147
Trappin Recidivism <austinslater58@gmail.com>
2023-02-03 09:51:01 -0600

    aligned certificates using grid layout

--- src/components/pages/Certificates/Certificates.js
+++ src/components/pages/Certificates/Certificates.js
@@ -11,30 +11,29 @@ import './Certificates.css'
 export default function Certificates() {
     return (
         <div>
-            <h1>Certificates</h1>
+            <h3>Certificates</h3>
             <div className="container">
-
-            </div>
-            <div className="img-container">
-                <img src={UofM} alt="uofm"></img>
-            </div>
-            <div className="img-container">
-                <img src={PhaserJS} alt="PhaserJS"></img>
-            </div>
-            <div className="img-container">
-                <img src={javaScript} alt="javaScript"></img>
-            </div>
-            <div className="img-container">
-                <img src={ReactCert} alt="ReactCert"></img>
-            </div>
-            <div className="img-container">
-                <img src={htmlCert} alt="htmlCert"></img>
-            </div>
-            <div className="img-container">
-                <img src={Cli} alt="Cli"></img>
-            </div>
-            <div className="img-container">
-                <img src={responsiveCSS} alt="responsiveCSS"></img>
+                <div id="uofmContainer"className="img-container">
+                    <img src={UofM} alt="uofm" id="uofmImg"></img>
+                </div>
+                <div className="img-container">
+                    <img src={PhaserJS} alt="PhaserJS"></img>
+                </div>
+                <div className="img-container">
+                    <img src={javaScript} alt="javaScript"></img>
+                </div>
+                <div className="img-container">
+                    <img src={ReactCert} alt="ReactCert"></img>
+                </div>
+                <div className="img-container">
+                    <img src={htmlCert} alt="htmlCert"></img>
+                </div>
+                <div className="img-container">
+                    <img src={Cli} alt="Cli"></img>
+                </div>
+                <div className="img-container">
+                    <img src={responsiveCSS} alt="responsiveCSS"></img>
+                </div>
             </div>
         </div>
     );
