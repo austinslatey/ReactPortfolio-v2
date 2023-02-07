9864a16f66d22d0e8241e7f2a23fb62dc00a38e5
Trappin Recidivism <austinslater58@gmail.com>
2023-02-07 17:25:49 -0600

    projects in projects page styled and ready to add new skill path

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -11,29 +11,29 @@ export default function Projects() {
             <div className="cardContainer">
                 <div className="card">
                     <div className="img-container">
-                    <img src={bugInvaders} alt="bugInv"></img>
+                        <img src={bugInvaders} alt="bugInv"></img>
                     </div>
                     <div className="card-text">
                         <p>Phaser.js Skill Path</p>
-                        <a href="https://austinslatey.github.io/bug-invaders/">Play Bug-Invaders</a>
+                        <a href="https://austinslatey.github.io/bug-invaders/">BugInvaders</a>
                     </div>
                 </div>
                 <div className="card">
-                <div className="img-container">
-                    <img src={cubeMatcher} alt="cubeMatch"></img>
+                    <div className="img-container">
+                        <img src={cubeMatcher} alt="cubeMatch"></img>
                     </div>
                     <div className="card-text">
                         <p>Phaser.js Skill Path</p>
-                        <a href="https://austinslatey.github.io/Cube-Matcher/">Play Cube-Matcher</a>
+                        <a href="https://austinslatey.github.io/Cube-Matcher/">CubeMatcher</a>
                     </div>
                 </div>
                 <div className="card">
-                <div className="img-container">
-                    <img src={winterWonderland} alt="winterWonderland"></img>
+                    <div className="img-container">
+                        <img src={winterWonderland} alt="winterWonderland"></img>
                     </div>
                     <div className="card-text">
                         <p>Phaser.js Skill Path</p>
-                        <a href="https://austinslatey.github.io/winter-wonderland/">Play Winter-Wonderland</a>
+                        <a href="https://austinslatey.github.io/winter-wonderland/">WinterWonderland</a>
                     </div>
                 </div>
             </div>
