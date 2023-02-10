7d3c82eafd1e78b31eddeade16b31842cda151a8
Trappin Recidivism <austinslater58@gmail.com>
2023-02-09 20:48:43 -0600

    added all elements and text to About me page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,7 +3,51 @@ import React from "react";
 export default function About() {
     return (    
         <div>
-            <h1>About</h1>
+            <h2>About me</h2>
+            <div className="about-container">
+                <div className="description-container">
+                    <p className="a-text">Hello! My name is Austin Slater, and I'm a Full-Stack Web Developer from Minnesota. Currently, I have the pleasure to be employed as an Instuctional Specialist for edX, helping students assisting students as they gradually learn Web Develpment.</p>
+                    <p className="b-text">On a typical day, I enjoy learning new concepts to further my education as a Web & Software Dev. Studying on CodeCademy and thoroughly reading documentaion have been my go-to learning platforms post graduation from University of Minnesota's Full-Stack Web 
+                        Development Bootcamp. Feel free to view the list of skills I've acquired along my journey.
+                    </p>
+                    <p className="c-text">My favorite activities are fishing in the Land of 10,000 Lakes, watching Rick & Morty, cooking foods from scratch, and playing Rocket Leauge on PC. Feel free to connect with me on LinkedIn using the icon below to reach out to me directly. Kind regards for visiting my portfolio, I hope your time here was plesant.</p>
+                </div>
+                <div className="skills-container">
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS & Bootstrap</li>
+                        <li>JavaScript</li>
+                        <li>Jquery</li>
+                        <li>Phaser.js</li>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                        <li>Document Object Model & Web APIs</li>
+                        <li>3rd Party Web APIs</li>
+                        <li>Server Side APIs</li>
+                        <li>HTML Handlebars</li>
+                        <li>React.js & State</li>
+                        <li>Login Authorization</li>
+                        <li>SQL</li>
+                        <li>Sequelize</li>
+                        <li>GraphQL</li>
+                        <li>Progressive Web Applications</li>
+                        <li>Git Bash</li>
+                        <li>Linux VM & Dual Boot</li>
+                        <li>Python</li>
+                        <li>Powershell</li>
+                        <li>C#</li>
+                        <li>Object Oriented Programming</li>
+                        <li>Object Relational Mapping</li>
+                        <li>Basics of Computer Science: JavaScript</li>
+                        <li>Basics of Computer Science: Python</li>
+                        <li>Regular Expressions</li>
+                        <li>Agile Project Management</li>
+                        <li>SCRUM Project Management</li>
+                        
+
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
