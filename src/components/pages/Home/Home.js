702b5bbbd79c6694a5b06669037623525c8dd201
Trappin Recidivism <austinslater58@gmail.com>
2023-02-12 18:39:09 -0600

    removed all unnessesary code from Home.js

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,15 +1,12 @@
 import React from "react";
-// import banner from './Assets/banner.png'
-// import selfie from './Assets/selfie.png'
+
 import './Home.css'
 
 export default function Home() {
     return (
         <div className="homeContainer">
             <h1>Elementum pulvinar etiam non quam lacus.</h1>
-        <div className="banner">
-        {/* <img src={selfie} alt="selfie"></img> */}
-            
+        <div className="banner">            
             <p>Amet nisl suscipit adipiscing bibendum est ultricies integer. Viverra maecenas accumsan lacus vel facilisis volutpat est velit. Mauris pellentesque pulvinar pellentesque habitant. Diam vel quam elementum pulvinar etiam non quam lacus suspendisse.
             </p>
         </div>
