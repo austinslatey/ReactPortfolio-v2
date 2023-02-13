83fe7867b35c98f5bcc9f3bd0c0cab52743ba50a
Trappin Recidivism <austinslater58@gmail.com>
2023-02-12 18:37:40 -0600

    new style added to portfolio mostly colors changed and margin/padding fixes

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,5 +1,4 @@
 import React from "react";
-import selfie from './assets/selfie.png'
 import './About.css'
 
 export default function About() {
@@ -8,52 +7,45 @@ export default function About() {
             <h2>About me</h2>
             <div className="about-container">
                 <div className="description-container">
-                    <p id="a-text" className="about-text">Hello! My name is Austin Slater, and I'm a Full-Stack Web Developer from Minnesota. Currently, I have the pleasure to be employed as an Instuctional Specialist for edX, assisting students as they gradually learn Web Develpment.</p>
-                    <p id="b-text" className="about-text">On a typical day, I enjoy learning new concepts to further educate myself in Web & Software dev, studying on CodeCademy and thoroughly reading documentaion have been my go-to. I have found these tools useful post graduation from University of Minnesota's Full-Stack Web
-                        Development Bootcamp. Feel free to view the list of skills I've acquired along my journey.
-                    </p>
-                    <p id="c-text" className="about-text">My favorite activities are fishing in the Land of 10,000 Lakes, watching Rick & Morty, cooking foods from scratch, and playing Rocket Leauge on PC. Feel free to connect with me on LinkedIn using the icon below to reach out to me directly. Kind regards for taking the time to visit my portfolio, I hope your stay here was plesant.</p>
+                    <p id="a-text" className="about-text">Hello! My name is Austin Slater, and I'm a Full-Stack Web Developer from Minnesota. Currently, I have the pleasure to be employed as an Instuctional Specialist for edX, assisting students as they gradually learn Web Develpment.On a typical day, I enjoy learning new concepts to further educate myself in Web & Software dev, studying on CodeCademy and thoroughly reading documentaion have been my go-to. I have found these tools useful post graduation from University of Minnesota's Full-Stack Web Development Bootcamp. Feel free to view the list of skills I've acquired along my journey.My favorite activities are fishing in the Land of 10,000 Lakes, watching Rick & Morty, cooking foods from scratch, and playing Rocket Leauge on PC. Feel free to connect with me on LinkedIn using the icon below to reach out to me directly. Kind regards for taking the time to visit my portfolio, I hope your stay here was plesant.</p>
+
                 </div>
-                <div className="selfie">
-                    <img src={selfie} alt="selfie"></img>
-                    <ul className="skills-container">
-                            <li>HTML5</li>
-                            <li>CSS & Bootstrap</li>
-                            <li>JavaScript</li>
-                            <li>Jquery</li>
-                            <li>Phaser.js</li>
-                            <li>Express.js</li>
-                            <li>Node.js</li>
-                            <li>Document Object Model & Web APIs</li>
-                            <li>3rd Party Web APIs</li>
-                            <li>Server Side APIs</li>
-                            <li>HTML Handlebars</li>
-                            <li>React.js & State</li>
-                            <li>Login Authorization</li>
-                            <li>SQL</li>
-                            <li>Sequelize</li>
-                            <li>GraphQL</li>
-                            <li>Progressive Web Applications</li>
-                            <li>Git Bash</li>
-                            <li>Linux VM & Dual Boot</li>
-                            <li>Python</li>
-                            <li>Powershell</li>
-                            <li>C#</li>
-                            <li>Object Oriented Programming</li>
-                            <li>Object Relational Mapping</li>
-                            <li>Basics of Computer Science: JavaScript</li>
-                            <li>Basics of Computer Science: Python</li>
-                            <li>Regular Expressions</li>
-                            <li>Agile Project Management</li>
-                            <li>SCRUM Project Management</li>
-                            <li>VS Code Text Editor</li>
-                            <li>VIM Text Editor</li>
-                            <li>Nano Text Editor</li>
-
-
-                        </ul>
+                <div className="skills-container">
+                    <ul>
+                        <li>HTML5</li>
+                        <li>CSS & Bootstrap</li>
+                        <li>JavaScript</li>
+                        <li>Jquery</li>
+                        <li>Phaser.js</li>
+                        <li>Express.js</li>
+                        <li>Node.js</li>
+                        <li>Document Object Model & Web APIs</li>
+                        <li>3rd Party Web APIs</li>
+                        <li>Server Side APIs</li>
+                        <li>HTML Handlebars</li>
+                        <li>React.js & State</li>
+                        <li>Login Authorization</li>
+                        <li>SQL</li>
+                        <li>Sequelize</li>
+                        <li>GraphQL</li>
+                        <li>Progressive Web Applications</li>
+                        <li>Git Bash</li>
+                        <li>Linux VM & Dual Boot</li>
+                        <li>Python</li>
+                        <li>Powershell</li>
+                        <li>C#</li>
+                        <li>Object Oriented Programming</li>
+                        <li>Object Relational Mapping</li>
+                        <li>Basics of Computer Science: JavaScript</li>
+                        <li>Basics of Computer Science: Python</li>
+                        <li>Regular Expressions</li>
+                        <li>Agile Project Management</li>
+                        <li>SCRUM Project Management</li>
+                        <li>VS Code Text Editor</li>
+                        <li>VIM Text Editor</li>
+                        <li>Nano Text Editor</li>
+                    </ul>
                 </div>
-
             </div>
         </div>
     );
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 // import banner from './Assets/banner.png'
+// import selfie from './Assets/selfie.png'
 import './Home.css'
 
 export default function Home() {
     return (
         <div className="homeContainer">
-        <div className="banner">
             <h1>Elementum pulvinar etiam non quam lacus.</h1>
+        <div className="banner">
+        {/* <img src={selfie} alt="selfie"></img> */}
+            
             <p>Amet nisl suscipit adipiscing bibendum est ultricies integer. Viverra maecenas accumsan lacus vel facilisis volutpat est velit. Mauris pellentesque pulvinar pellentesque habitant. Diam vel quam elementum pulvinar etiam non quam lacus suspendisse.
             </p>
         </div>
