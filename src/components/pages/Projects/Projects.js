b659a98d58c5c2aa77c2ab54ff0c39d541801864
Trappin Recidivism <austinslater58@gmail.com>
2023-03-25 21:43:16 -0500

    moved Projects/assets imgs to its own folder and create seperate file to contain all object data refactored Projects.js and Projects.css Project.css has a few minor changes, mainly adding some additional styling to the links making them more visually distinct.

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -1,103 +1,24 @@
 import React from "react";
-import './Projects.css'
-import bugInvaders from './assets/bugInvaders.png'
-import winterWonderland from './assets/winterWonderland.png'
-import cubeMatcher from './assets/cubeMatcher.png'
-import concertScout from './assets/concertScout.png'
-import travelBug from './assets/travelBug.png'
-import recipEase from './assets/recipEase.png'
-import jamming from './assets/jamming.png'
-import catWorx from './assets/catWorx.png'
-import tsunamiCoffee from './assets/tsunamiCoffee.png'
-
+import "./Projects.css";
+import projectsData from "./assets/js/projectsData";
 
 export default function Projects() {
-    return (
-        <div>
-            <h2>Projects</h2>
-            <div className="cardContainer">
-                <div className="card">
-                    <div className="img-container">
-                        <img src={bugInvaders} alt="bugInv"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Phaser.js Skill Path</p>
-                        <a href="https://austinslatey.github.io/bug-invaders/">BugInvaders</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={cubeMatcher} alt="cubeMatch"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Phaser.js Skill Path</p>
-                        <a href="https://austinslatey.github.io/Cube-Matcher/">CubeMatcher</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={winterWonderland} alt="winterWonderland"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Phaser.js Skill Path</p>
-                        <a href="https://austinslatey.github.io/winter-wonderland/">WinterWonderland</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={concertScout} alt="concertScout"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Full Stack Bootcamp: Group Projects</p>
-                        <a href="https://austinslatey.github.io/concert-scout/">ConcertScout</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={travelBug} alt="travelBug"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Full Stack Bootcamp: Group Projects</p>
-                        <a href="https://github.com/Metelak/Travel-bug">TravelBug</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={recipEase} alt="recipEase"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Full Stack Bootcamp: Group Projects</p>
-                        <a href="https://github.com/austinslatey/RecipEase">RecipEase</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={jamming} alt="jamming"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Side Project</p>
-                        <a href="https://bucolic-baklava-e825fb.netlify.app">Jamming</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={catWorx} alt="catWorx"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Side Project</p>
-                        <a href="https://github.com/austinslatey/Catwork-Security">CatWorx</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="img-container">
-                        <img src={tsunamiCoffee} alt="tsunamiCoffee"></img>
-                    </div>
-                    <div className="card-text">
-                        <p>Side Project</p>
-                        <a href="https://austinslatey.github.io/Tsunami-Coffee/">TsunamiCoffee</a>
-                    </div>
-                </div>
+  return (
+    <div>
+      <h2>Projects</h2>
+      <div className="cardContainer">
+        {projectsData.map((project) => (
+          <div className="card" key={project.id}>
+            <div className="img-container">
+              <img src={project.image} alt={project.title}></img>
             </div>
-        </div>
-    );
+            <div className="card-text">
+              <p>{project.description}</p>
+              <a href={project.link}>{project.title}</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/components/pages/Projects/assets/js/projectsData.js
@@ -0,0 +1,79 @@
+import bugInvaders from '../imgs/bugInvaders.png';
+import winterWonderland from '../imgs/winterWonderland.png';
+import cubeMatcher from '../imgs/cubeMatcher.png';
+import concertScout from '../imgs/concertScout.png';
+import travelBug from '../imgs/travelBug.png';
+import recipEase from '../imgs/recipEase.png';
+import jamming from '../imgs/jamming.png';
+import catWorx from '../imgs/catWorx.png';
+
+
+
+const projectsData = [
+    {
+        id: 1,
+        title: 'BugInvaders',
+        description: 'A Space Invaders-style arcade game built with Phaser.js',
+        image: bugInvaders,
+        link: 'https://austinslatey.github.io/bug-invaders/'
+    },
+    {
+        id: 2,
+        title: 'CubeMatcher',
+        description: 'A puzzle game built with Phaser.js',
+        image: cubeMatcher,
+        link: 'https://austinslatey.github.io/Cube-Matcher/'
+    },
+    {
+        id: 3,
+        title: 'WinterWonderland',
+        description: 'A holiday-themed platformer built with Phaser.js',
+        image: winterWonderland,
+        link: 'https://austinslatey.github.io/winter-wonderland/'
+    },
+    {
+        id: 4,
+        title: 'ConcertScout',
+        description: 'A concert and music festival discovery app built with React and Firebase',
+        image: concertScout,
+        link: 'https://austinslatey.github.io/concert-scout/'
+    },
+    {
+        id: 5,
+        title: 'TravelBug',
+        description: 'A travel journal app built with React, Node.js, and MongoDB',
+        image: travelBug,
+        link: 'https://github.com/Metelak/Travel-bug'
+    },
+    {
+        id: 6,
+        title: 'RecipEase',
+        description: 'A recipe sharing app built with React, Node.js, and PostgreSQL',
+        image: recipEase,
+        link: 'https://github.com/austinslatey/RecipEase'
+    },
+    {
+        id: 7,
+        title: 'Jamming',
+        description: 'A music discovery app built with React and the Spotify API',
+        image: jamming,
+        link: 'https://bucolic-baklava-e825fb.netlify.app'
+    },
+    {
+        id: 8,
+        title: 'CatWorx',
+        description: 'A cat-themed security company website built with HTML, CSS, and JavaScript',
+        image: catWorx,
+        link: 'https://github.com/austinslatey/Catwork-Security'
+    },
+    // {
+    //     id: 9,
+    //     title: 'TsunamiCoffee',
+    //     description: 'A coffee shop website built with HTML, CSS, and JavaScript',
+    //     image: tsunamiCoffee,
+    //     link: 'https://austinslatey.github.io/Tsunami-Coffee/'
+    // },
+
+];
+
+export default projectsData;
